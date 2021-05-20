c579a308a893855035d9fa9c8992efe0e4c47d04
benoitsemifir <62873435+benoitsemifir@users.noreply.github.com>
2021-05-20 11:37:13 +0200

    Complétion Readme

--- app/Voiture.ts
+++ app/Voiture.ts
@@ -1,19 +1,25 @@
 // "Moule" ou modéle d'objet
 export class Voiture {
     // attribut d'une classe (variables)
-    couleur: string;
-    marque: string;
-    private _nbRoues: number;
+    public couleur: string;
+    public marque: string;
+    private _nbRoues!: number;
+    // mot clé static: rend un attribut ou une méthode partagé entre les instances de classes (entre tous les objets)
+    public static compteur:number = 0;
 
     // constructor est la méthode appelée quand on fait "new Voiture()"
-    constructor(couleur: string, marque: string, nbRoues: number = 4) {
+    public constructor(couleur: string, marque: string, nbRoues: number = 4) {
         this.couleur = couleur;
         this.marque = marque;
-        if(nbRoues>0 && nbRoues<5){
-            this._nbRoues = nbRoues;
-        }else{
-            this._nbRoues = 4;
-        }
+        // if(nbRoues>0 && nbRoues<5){
+        //     this._nbRoues = nbRoues;
+        // }else{
+        //     this._nbRoues = 4;
+        // }
+        this.setNbRoues(nbRoues);
+        this.check();
+        // pas d'utilisation du this pour les attributs static, ils concernent la classe et pas l'objet
+        Voiture.compteur++;
     }
     //accesseur (getter): permet de récupérer un attribut de manière sécurisée.
     get nbRoues(): number{
@@ -22,7 +28,16 @@ export class Voiture {
     }
 
     // mutateur (setter): permet de changer la valeur d'un attribut de manière sécurisée.
-    set nbRoues(nbRoues: number){
+    // set nbRoues(nbRoues: number){
+        // if(nbRoues>0 && nbRoues<5){
+        //     this._nbRoues = nbRoues;
+        // }else{
+        //     this._nbRoues = 4;
+        // }
+    // }
+
+    // setter façon Java / C++ etc. Utilisable dans le consctructeur
+    public setNbRoues(nbRoues: number) {
         if(nbRoues>0 && nbRoues<5){
             this._nbRoues = nbRoues;
         }else{
@@ -33,8 +48,26 @@ export class Voiture {
 
     // méthode de la classe voiture (fonction)
     // cette méthode est propre à l'objet (elle concerne seulement l'objet qui l'appelle)
-    rouler(distance: number): void{
+    public rouler(distance: number): void{
+        this.check()
         console.log(`Je suis une voiture ${this.couleur} et je roule pendant ${distance} km`)
+    }
+
+    // méthode privée: je ne peux l'appeler qu'au sein de la classe
+    private check(){
+        if (this.nbRoues > 0 && this.nbRoues< 5) {
+            console.log("Le nombre de roues est correct");
+        }else{
+            console.log("Il y a un problème avec le nombre de roues, je ne peux pas démarer")
+        }
+    }
+
+    /**
+     * Méthode pour décrire une voiture.
+     * @param voiture La voiture à décrire.
+     */
+    public static décrireVoiture(voiture: Voiture){
+        console.log(`Voiture de marque ${voiture.marque} de couleur ${voiture.couleur} avec ${voiture.nbRoues} roue(s)`);
     }
 }
 
@@ -43,3 +76,4 @@ export class Voiture {
 // const voitureRouge1 = new Voiture();
 // const voitureRouge2 = new Voiture();
 // const voitureRouge3 = new Voiture();
+
--- app/index.ts
+++ app/index.ts
@@ -1,14 +1,27 @@
 import { Voiture } from "./Voiture";
 
 // instance de la classe Voiture
+console.log("Nombre de voiture: " + Voiture.compteur);
 const voiture1 = new Voiture("Rouge", "Renault");
+console.log("Nombre de voiture: " + Voiture.compteur);
 const voiture2 = new Voiture("Bleu", "Peugeot", 5);
+console.log("Nombre de voiture: " + Voiture.compteur);
+
 console.log({voiture1, voiture2});
 // appel du mutateur (avec sécuritée)
-voiture2.nbRoues = -5;
+
+voiture2.setNbRoues(-5);
 console.log({voiture1, voiture2});
+
 // appel de l'accesseur
 console.log(voiture2.nbRoues);
+
 // appels de méthodes des objets Voiture
 voiture1.rouler(10);
 voiture2.rouler(5);
+
+// erreur: utilisation d'une méthode privée en dehors de la classe
+// voiture2.check();
+
+// appel de méthode static depuis la classe voiture
+Voiture.décrireVoiture(voiture1);
